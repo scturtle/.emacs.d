65438494dd93114e5802a3a433d483a37e7e8bae
scturtle <scturtle@gmail.com>
2025-11-18 10:58:58 +0800

    remove github proxy

--- llvm.sh
+++ llvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-export LLVMURL=https://ghp.ci/https://raw.githubusercontent.com/llvm/llvm-project/refs/heads/main
+export LLVMURL=https://raw.githubusercontent.com/llvm/llvm-project/refs/heads/main
 export LLVMDIR=$HOME/.emacs.d/lisp/llvm-utils
 mkdir -p $LLVMDIR
 curl $LLVMURL/llvm/utils/emacs/tablegen-mode.el   -o $LLVMDIR/tablegen-mode.el
